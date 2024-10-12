c0dcd10603bf440652bfbd8996dcb103b02ea0d4
victo29 <victavares2019@gmail.com>
2024-10-12 20:50:52 -0300

    Authentication with Flask Login

--- src/app.py
+++ src/app.py
@@ -1,17 +1,29 @@
 from flask import Flask
+from flask_login import LoginManager
+from models.conection import get_db  # Presumindo que você tenha esta função
 from controllers.UsuarioController import usuario_bp
-from controllers.PublicacaoController import publicacao_bp
+from models.usuario import Usuario
+# from controllers.PublicacaoController import publicacao_bp
 
 app = Flask(__name__)
-
-#  chave -> sha256
-
-
-app = Flask(__name__, template_folder='views')
 app.register_blueprint(usuario_bp)
-app.register_blueprint(publicacao_bp)
+# app.register_blueprint(publicacao_bp)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Define a rota de login
+login_manager.login_view = 'usuario_bp.logar'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    # Aqui você deve carregar o usuário do banco de dados
+    with get_db() as session:  # Use a função de obter a sessão que você definiu
+        return session.query(Usuario).filter_by(id=user_id).first()
 
 
 if __name__ == "__main__":
+
     app.secret_key = 'bb5d3680c0d90478ba469ff4a12b09b59df7525cd54b2e1235ddcd85bf2a5047' 
     app.run(debug=True)
--- src/controllers/UsuarioController.py
+++ src/controllers/UsuarioController.py
@@ -1,85 +1,67 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+from flask_login import login_user, logout_user, current_user, login_required
+from models.usuario import Usuario
+
+
 import sys
 from pathlib import Path
+
 file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session
-from models.usuario import Usuario
+usuario_bp = Blueprint('usuario_bp', __name__)
 
-
-
-usuario_bp = Blueprint( 'usuario_bp',__name__)
-
-
-@usuario_bp.route('/cadastrar', methods=['GET'])
-def get_cadastrar():
-
-    return render_template('cadastro.html')
-    
-
-
-@usuario_bp.route('/', methods=['GET'])
-@usuario_bp.route('/logar', methods=['GET'])
-def get_logar():
-    return render_template('login.html')
-    
-
-@usuario_bp.route('/home', methods=['GET'])
-def get_home():
-    if 'logedin' in session:
-        return render_template('home.html')
-    else:
-        return redirect(url_for('usuario_bp.get_logar'), code=302)
-    
-@usuario_bp.route('/publicar', methods=['GET'])
-def get_publicar():
-    if 'logedin' in session:
-        return render_template('publicar.html')
-    else:
-        return redirect(url_for('usuario_bp.get_logar'), code=302)
-
-
-@usuario_bp.route('/get-dados-usuario', methods=['GET'])
-def get_dados_usuario():
-    return jsonify(session['usuario'])
-
-
-
-
-@usuario_bp.route('/cadastrar-post', methods=['POST'])
+@usuario_bp.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
+    if request.method == 'GET':
+        return render_template('register.html')
     
-    nome = request.form.get('nome')
+    # POST
+    name = request.form.get('name')
     email = request.form.get('email')
-    senha = request.form.get('senha')
+    pwd = request.form.get('password')
 
-    if not nome or not email or not senha:
-        return jsonify({"error": "Todos os campos são obrigatórios"}), 400  # Provavelmente não será necessário
+    if not name or not email or not pwd:
+        return jsonify({"error": "Todos os campos são obrigatórios"}), 400
 
     try:
-        novo_usuario = Usuario(email,senha,nome)
-        novo_usuario.cadastrar()
+        novo_usuario = Usuario(email=email, password=pwd, name=name)
+        novo_usuario.register()  
     except Exception as error:
-        return jsonify({"error": error.args[0]}), 400
-        
+        return jsonify({"error": str(error)}), 400
 
     return jsonify({"message": "Usuário cadastrado com sucesso!"}), 201
 
-
-@usuario_bp.route('/logar-post', methods=['POST'])
+@usuario_bp.route('/', methods=['GET', 'POST'])
+@usuario_bp.route('/logar', methods=['GET', 'POST'])
 def logar():
+    if request.method == 'GET':
+        return render_template('login.html')
 
+    # POST
     email = request.form.get('email')
-    senha = request.form.get('senha')
+    pwd = request.form.get('password')
 
-    if not email or not senha:
-        return jsonify({"error": "Todos os campos são obrigatórios"}), 400 # Provavelmente não será necessário
-    
+    if not email or not pwd:
+        return jsonify({"error": "Todos os campos são obrigatórios"}), 400
+
     try:
-        usuario_logado = Usuario(email,senha)
-        usuario_logado.logar()
+        usuario_logado = Usuario(email=email, password=pwd)
+        user = usuario_logado.login() 
+        login_user(user) 
+        return jsonify({"redirect": url_for('usuario_bp.get_home')})  
     except Exception as error:
-        return jsonify({"error": error.args}), 400
+        return jsonify({"error": str(error)}), 400
     
-    return jsonify({"redirect": url_for('usuario_bp.get_home')})
+@usuario_bp.route('/home', methods=['GET'])
+@login_required  
+def get_home():
+    return render_template('home.html', nome=current_user.name)
+
+
+@usuario_bp.route('/logout', methods=['POST'])
+@login_required  
+def logout():
+    logout_user()  
+    return redirect(url_for('usuario_bp.get_logar'))
